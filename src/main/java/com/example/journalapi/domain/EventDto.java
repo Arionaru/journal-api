6c6791db625312605de2ff986c5e27f09c8c26e0
Ariona <e.petrushenko@inbox.ru>
2022-05-05 17:14:48 +0300

    add constructors to EventDto

--- src/main/java/com/example/journalapi/domain/EventDto.java
+++ src/main/java/com/example/journalapi/domain/EventDto.java
@@ -1,13 +1,17 @@
 package com.example.journalapi.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventDto {
     private String source;
     private String sourceId;
